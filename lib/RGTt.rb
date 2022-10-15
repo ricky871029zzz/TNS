d504922c074330e0625b8f0e089b9e260c90dcca
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-10-16 06:04:07 +0800

    Google Trend

---
+++ lib/RGTt.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module CodePraise
+  class RGTt
+    def initialize(rgt_data, rgt_source)
+      @rgt = rgt_data
+      @rgt_source = rgt_source
+    end
+  end
+end
