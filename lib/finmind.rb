8cefe831437d84440496263dd7f297d8d0838195
ZweiChen0328 <maxchen0328@gmail.com>
2022-10-16 23:37:39 +0800

    create spec

--- lib/finmind.rb
+++ lib/finmind.rb
@@ -2,9 +2,14 @@
 
 module CodePraise
   class FinMind
+
     def initialize(stock_data, data_source)
       @stock = stock_data
       @data_source = data_source
+    end
+
+    def get_stock
+      @stock
     end
   end
 end
--- spec/fm_api_spec.rb
+++ spec/fm_api_spec.rb
@@ -2,3 +2,26 @@ require 'minitest/autorun'
 require 'minitest/rg'
 require 'yaml'
 require_relative '../lib/finmind_api.rb'
+
+CORRECT = YAML.safe_load(File.read('fixtures/finmind_results.yml'))
+
+describe 'Tests FinMind API library' do
+  stock = CodePraise::FinMindApi.new
+  stock.dataset("TaiwanStockInfo")
+  describe 'Status' do
+    it 'stock_dataset' do
+      _(stock.stock.get_stock["status"]).must_equal CORRECT["status"]
+    end
+  end
+  describe 'Msg' do
+    it 'stock_dataset' do
+       _(stock.stock.get_stock["msg"]).must_equal CORRECT["msg"]
+    end
+  end
+  describe 'Data' do
+    it 'stock_dataset' do
+       _(stock.stock.get_stock["data"]).must_equal CORRECT["data"]
+    end
+  end
+end
+      
