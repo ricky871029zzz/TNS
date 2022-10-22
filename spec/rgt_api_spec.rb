f058ff1373e72b3c58f45eed5f5fd9c23c6b2679
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-10-22 20:21:48 +0800

    Add files via upload

--- spec/rgt_api_spec.rb
+++ spec/rgt_api_spec.rb
@@ -1,25 +1,38 @@
-require 'minitest/autorun'
-require 'minitest/rg'
-require 'yaml'
-require_relative 'lib/RGT.rb'
+# frozen_string_literal: true
 
-CORRECT = YAML.safe_load(File.read('/Users/yehanzhang/Desktop/RGT/spec/fixture/rgt_results.yml'))
+require_relative 'spec_helper'
+
+
 
 describe 'Tests RGT API library' do
-  p = CodePraise::RGT.new("TSMC")
+  VCR.configure do |c|
+    c.cassette_library_dir= CASSETTES_FOLDER
+    c.hook_into:webmock
+    c.filter_sensitive_data('<RGT_TOKEN>') { RGT_TOKEN }
+  end
+  
+  before do
+    VCR.insert_cassette CASSETTE_FILE, record: :new_episodes, match_requests_on: %i[method uri headers]
+  end
+  
+  after do
+  VCR.eject_cassette
+  end
+
+  p = CodePraise::RGT.new('TSMC')
   describe 'search_metadata' do
     it 'search_metadata' do
-      _(p.get_jason.get_rgt["search_metadata"]).must_equal CORRECT["search_metadata"]
+      _(p.get_jason.get_rgt['search_metadata']).must_equal CORRECT['search_metadata']
     end
   end
   describe 'search_parameters' do
     it 'search_parameters' do
-       _(p.get_jason.get_rgt["search_parameters"]).must_equal CORRECT["search_parameters"]
+      _(p.get_jason.get_rgt['search_parameters']).must_equal CORRECT['search_parameters']
     end
   end
   describe 'interest_over_time' do
     it 'interest_over_time' do
-       _(p.get_jason.get_rgt["interest_over_time"]).must_equal CORRECT["interest_over_time"]
+      _(p.get_jason.get_rgt['interest_over_time']).must_equal CORRECT['interest_over_time']
     end
   end
 end
---
+++ spec/spec_helper_rgt.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
+
+require 'yaml'
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'vcr'
+require 'webmock'
+
+require_relative '../lib/RGT.rb'
+CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
+RGT_TOKEN=CONFIG['api_key']
+CORRECT = YAML.safe_load(File.read('spec/fixtures/rgt_results.yml'))
+
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'
+CASSETTE_FILE = 'rgt_api'
