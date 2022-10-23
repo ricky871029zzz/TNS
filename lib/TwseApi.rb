6cbb4fded780368957449945601adf595420a299
ricky871029 <62226849+ricky871029zzz@users.noreply.github.com>
2022-10-24 01:20:25 +0800

    Update TwseApi.rb

--- lib/TwseApi.rb
+++ lib/TwseApi.rb
@@ -9,43 +9,57 @@ module CodePraise
     API_PROJECT_ROOT = 'https://openapi.twse.com.tw/v1/'
     # https://openapi.twse.com.tw/v1/exchangeReport/TWT84U -> 以字典為元素組成之一維陣列
     # https://openapi.twse.com.tw/v1/exchangeReport/MI_INDEX20 
-    module Errors
-      class NotFound < StandardError; end
-      class Unauthorized < StandardError; end
-    end
-    
-    HTTP_ERROR = {
-      401 => Errors::Unauthorized,
-      404 => Errors::NotFound
-    }.freeze
 
     def twse_single(stock_symbol)
-      project_req_url = twse_api_path('exchangeReport/TWT84U')
-      stock_data = call_twse_url(project_req_url).parse # 回傳的result是json，parse後是hash or array
+      res = Request.new(API_PROJECT_ROOT).path_complete('exchangeReport/TWT84U')
+      stock_data = res.parse # 回傳的result是json，parse後是hash or array
       stock_data = stock_data.select { |x| x['Code'] == "#{stock_symbol}" }
       stock_data = stock_data[0]
       TwseSingle.new(stock_data, self)
     end
 
     def twse_rank
-      project_req_url = twse_api_path('exchangeReport/MI_INDEX20')
-      rank_data = call_twse_url(project_req_url).parse # 回傳的result是json，parse後是hash or array
+      res = Request.new(API_PROJECT_ROOT).path_complete('exchangeReport/MI_INDEX20')
+      rank_data = res.parse # 回傳的result是json，parse後是hash or array
       TwseRank.new(rank_data, self)
     end
-    private
 
-    def twse_api_path(para)
-      "#{API_PROJECT_ROOT}#{para}" 
+    # Sends out HTTP requests to TWSE
+    class Request
+      def initialize(resource_root)
+        @resource_root = resource_root
+      end
+
+      def path_complete(para)
+        get(@resource_root + para)
+      end
+
+      def get(url)
+        http_response = HTTP.get(url)
+
+        Response.new(http_response).tap do |response|
+          raise(response.error) unless response.successful?
+        end
+      end
     end
 
-    def call_twse_url(url)
-      result = HTTP.get(url)
-      successful?(result) ? result : raise(HTTP_ERROR[result.code])
-    end
-    # result
+    # Decorates HTTP responses with success/error
+    class Response < SimpleDelegator
+      Unauthorized = Class.new(StandardError)
+      NotFound = Class.new(StandardError)
 
-    def successful?(result)
-      !HTTP_ERROR.keys.include?(result.code)
+      HTTP_ERROR = {
+        401 => Unauthorized,
+        404 => NotFound,
+      }.freeze
+
+      def successful?
+        HTTP_ERROR.keys.none?(code)
+      end
+
+      def error
+        HTTP_ERROR[code]
+      end
     end
   end
 end
